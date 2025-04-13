e7a217b71deaceca1a7c92b180137efd05ca93ca
MomusWinner <areg06.06@mail.ru>
2025-04-14 00:24:02 +0300

    Implement base card logic

---
+++ game-client/MicroDurak/main/card/card.go
@@ -0,0 +1,15 @@
+components {
+  id: "card"
+  component: "/main/card/card.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"Clovers_2_black\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/images/cards.atlas\"\n"
+  "}\n"
+  ""
+}
