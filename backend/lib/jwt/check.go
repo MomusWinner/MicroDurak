ff6a64285a30015dde68907c642f0c8ce0171967
spl3g <spleefer6@yandex.ru>
2025-04-14 12:16:20 +0300

    Combine decoding and checking tocken

--- backend/lib/jwt/check.go
+++ backend/lib/jwt/check.go
@@ -1,11 +1,22 @@
 package jwt
 
 import (
+	"encoding/base64"
 	"fmt"
+
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func VerifyToken(publicKey string, tokenString string) (jwt.Claims, error) {
+func VerifyToken(PubKeyEncoded string, tokenString string) (jwt.Claims, error) {
+	pemBytes, err := base64.StdEncoding.DecodeString(PubKeyEncoded)
+	if err != nil {
+		return nil, err
+	}
+	publicKey, err := jwt.ParseRSAPublicKeyFromPEM(pemBytes)
+	if err != nil {
+		return nil, err
+	}
+
 	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
