3ebe0f3e24a20e11555d44cb05d7fd3d649ac4d4
MomusWinner <areg06.06@mail.ru>
2025-04-14 16:04:07 +0300

    Update registration gui. Connect to auth api

---
+++ game-client/MicroDurak/main/request.lua
@@ -0,0 +1,38 @@
+local HOST = "http://localhost:8080/"
+
+local M = {}
+
+---@param user table
+---@param handler function
+function M.register(user, handler)
+	assert(user.name)
+	assert(user.age)
+	assert(user.email)
+	assert(user.password)
+
+	print("REQUEST register")
+	local url = HOST .. "register"
+	local method = "POST"
+	local header = { ["Content-Type"] = "application/json" }
+	local body = json.encode(user)
+
+	http.request(url, method, handler, header, body)
+end
+
+---@param user table
+---@param handler function
+function M.login(user, handler)
+	assert(user.email)
+	assert(user.password)
+
+	print("REQUEST login")
+	local url = HOST .. "login"
+	local method = "POST"
+	local header = { ["Content-Type"] = "application/json" }
+	local body = json.encode(user)
+	print(boyd)
+
+	http.request(url, method, handler, header, body)
+end
+
+return M
