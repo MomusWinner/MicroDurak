3d7e96a3749743be2d03bc93b71ad8a4b61480d4
spl3g <spleefer6@yandex.ru>
2025-04-26 19:35:46 +0300

    players: implement the CreateMatchResult method

--- backend/services/players/methods.go
+++ backend/services/players/methods.go
@@ -8,6 +8,7 @@ import (
 	"github.com/MommusWinner/MicroDurak/internal/database"
 	"github.com/MommusWinner/MicroDurak/internal/players/v1"
 	"github.com/MommusWinner/MicroDurak/services/players/config"
+	"github.com/MommusWinner/MicroDurak/services/players/rating"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -74,5 +75,64 @@ func playerGameResultToString(gr players.GameResult) string {
 }
 
 func (ps *PlayerService) CreateMatchResult(ctx context.Context, req *players.CreateMatchResultRequest) (*players.CreateMatchResultResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateMatchResult not implemented")
+	tx, err := ps.DBConn.Begin(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback(ctx)
+
+	dbQueries := ps.DBQueries.WithTx(tx)
+
+	match, err := ps.DBQueries.CreateMatchResult(ctx, database.CreateMatchResultParams{
+		PlayerCount: int16(len(req.PlayerPlacements)),
+		GameResult:  database.GameResult(playerGameResultToString(req.GameResult)),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	playerScores, err := rating.CalculatePlayerScores(ctx, dbQueries, req.PlayerPlacements)
+	if err != nil {
+		return nil, err
+	}
+
+	playerRatings := make([]*players.PlayerPlacementResponse, len(req.PlayerPlacements))
+	for i, player := range req.PlayerPlacements {
+		playerId, err := uuid.Parse(player.PlayerId)
+		if err != nil {
+			return nil, status.New(codes.InvalidArgument, "player_id is not a uuid").Err()
+		}
+		playerDbId := pgtype.UUID{Bytes: playerId, Valid: true}
+
+		playerRating := playerScores[player.PlayerId].NewRating
+		playerRatingChange := int32(playerScores[player.PlayerId].RatingChange)
+
+		_, err = dbQueries.AddPlayerPlacement(ctx, database.AddPlayerPlacementParams{
+			MatchResultID: match.ID,
+			PlayerID:      playerDbId,
+			RatingChange:  playerRating,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = dbQueries.UpdatePlayerRating(ctx, database.UpdatePlayerRatingParams{
+			ID:     playerDbId,
+			Rating: playerRating,
+		})
+
+		playerRatings[i] = &players.PlayerPlacementResponse{
+			PlayerId:           playerId.String(),
+			PlayerRating:       playerRating,
+			PlayerRatingChange: playerRatingChange,
+		}
+	}
+	response := &players.CreateMatchResultResponse{MatchResultId: match.ID.String(), PlayerRatings: playerRatings}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
 }
